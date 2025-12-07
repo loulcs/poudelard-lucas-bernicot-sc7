f8b9d6ea88c40a20791d493070fa942d2a95bbf2
loulcs <lou.lucas@efrei.net>
2025-12-07 10:23:06 +0100

    message

--- utils/input_utils.py
+++ utils/input_utils.py
@@ -1,6 +1,15 @@
 def demander_texte(message):
-    texte=input(message)
-    while texte==" ":
-        texte=input(message)
+    texte=input(message).strip()
+    while texte=="":
+        texte=input(message).strip()
     return texte
 def demander_nombre(message, min_val=None, max_val=None):
+        val = input(message).strip())
+        if min_val is None and max_val is None:
+            return val
+        elif min_val is not None and max_val is None :
+            if val >= min_val :
+                return val
+
+
+
