8f5749d46eaf6ffe6bcce7f2f5d278fb8946a7bb
loulcs <lou.lucas@efrei.net>
2025-12-23 16:11:15 +0100

    depot intermédiaire

--- chapitres/chapitre_3.py
+++ chapitres/chapitre_3.py
@@ -1,7 +1,10 @@
 import json
 import random
 
-def apprendre_sorts(joueur, chemin_fichier="data/sort.json"):
+from univers.maison import actualiser_points_maison
+
+
+def apprendre_sorts(joueur, chemin_fichier="data/sorts.json"):
     # Chargement des sorts depuis le fichier JSON
     with open(chemin_fichier, "r", encoding="utf-8") as f:
         tous_sorts = json.load(f)
@@ -32,6 +35,7 @@ def apprendre_sorts(joueur, chemin_fichier="data/sort.json"):
     print("Voici les sortilèges que tu maîtrises désormais :")
     for sort in sort_selectionnes:
         print(f"- {sort['nom']} ({sort['type']}) : {sort['description']}")
+
     return joueur
 
 
@@ -60,17 +64,15 @@ def quiz_magie(joueur, chemin_fichier="data/quiz_magie.json"):
             print(f"Mauvaise réponse. La bonne réponse était : {q['reponse']}\n")
 
     print(f"Score obtenu : {score_quiz} points")
-    return score_quiz
 
 from univers.maison import actualiser_points_maison
 from univers.maison import afficher_maison_gagnante
 from univers.personnage import afficher_personnage
 
-def lancer_chapitre_3(personnage, maison):
+def lancer_chapitre_3(joueur, maison):
     apprendre_sorts(maison)
     quiz_magie(maison)
     actualiser_points_maison(maison)
     afficher_maison_gagnante(maison)
-    afficher_personnage(personnage)
-    return
+    afficher_personnage(joueur)
 
