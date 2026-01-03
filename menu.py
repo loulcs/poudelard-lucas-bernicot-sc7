af9fd94da239623c281f8d975b9fa211462b708d
loulcs <lou.lucas@efrei.net>
2026-01-03 11:22:50 +0100

    correction menu

--- menu.py
+++ menu.py
@@ -2,6 +2,7 @@ from chapitres.chapitre_4 import lancer_chapitre_4_quidditch
 from chapitres.chapitre_3 import lancer_chapitre_3
 from chapitres.chapitre_2 import lancer_chapitre_2
 from chapitres.chapitre_1 import lancer_chapitre_1
+from input_utils import demander_choix
 from univers.personnage import initialiser_personnage
 def afficher_menu_principal():
     print("1. Lancer le Chapitre 1 - L'arrivée dans le monde magique.")
@@ -12,16 +13,14 @@ def lancer_choix_menu():
     maisons = {"Gryffondor": 0,"Serpentard": 0,"Poufsouffle": 0,"Serdaigle":0}
     while True:
         menu = afficher_menu_principal()
-        choix=input("Votre choix :")
+        choix=demander_choix("Votre choix ?",["1","2"])
         if choix =="1":
             joueur=lancer_chapitre_1()
             lancer_chapitre_2(joueur)
             lancer_chapitre_3(joueur,maisons)
             lancer_chapitre_4_quidditch(joueur,maisons)
-            print("felicitation vous avez termine votre aventure a Poudlard")
+            print("Félicitation vous avez terminé votre aventure à Poudlard")
             break
-         elif choix=="2":
+        else:
              print("Vous avez quitte le jeu")
              break
-        else:
-            print("choix invalide veuillez recommencer")
